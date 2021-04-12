7c258a09447c1fc80071b1d2256add00a33e2d81
LucasSarri <lucassarri@hotmail.com>
2021-04-11 23:31:36 -0300

    alteracoes dia 11/04

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # PCII
 Repositório com arquivos da matéria de Programação de Computadores 2
 
-## Conteúdo e Anotações
-
 ## Execução dos programas
 1. Acessar a pasta do programa via CMD 
 2. Compilar o código usando gcc nome_arquivo.c -o nome_arquivo.exe
--- recordando.c
+++ recordando.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+#define teste 9; //Definindo constante
+
 int main (int argc, char *argv[])
 {
-	printf("Alo");
-	return 0;
+	//Exemplo de uso da função de saída printf
+	printf("Teste");
+	printf("%d",5);
+	x = 5;
+	printf("%d",x);
+	//Exemplo de uso da função de entrada
+	scanf("%d", &x) //Recebe o valor de x digitado pelo usuário
 }
